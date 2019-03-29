b3260a1bac8b4b80c892d294f6ee71bf371e813f
Bryophite <bryant.perkins.stem@gmail.com>
2019-03-29 11:53:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cake/FlashCards.kt
@@ -0,0 +1,13 @@
+package com.example.cake
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FlashCards : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.flashcard_layout)
+
+    }
+}
---
+++ app/src/main/java/com/example/cake/ViewSet.kt
@@ -0,0 +1,14 @@
+package com.example.cake
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewSet : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
