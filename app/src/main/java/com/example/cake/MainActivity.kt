a797fced56b85796698158a04ab12baa63b5a3e6
Bryophite <bryant.perkins.stem@gmail.com>
2019-03-30 09:09:11 -0500

    ERRTHANG'S Different :)

--- app/src/main/java/com/example/cake/MainActivity.kt
+++ app/src/main/java/com/example/cake/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.cake
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity(){
 
@@ -9,8 +12,24 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var set: List<String>
-
+        val setList = HashMap<String,String>()
+        addBtn.setOnClickListener{
+            val vocabWord = vocabWordBox.text.toString()
+            val vocabDef = definitionBox.text.toString()
+            val currItems = setItems.text.toString()
+            val newItems = "Word: $vocabWord Definition: $vocabDef \r\n"
+            val items: String
+            setList[vocabWord] = vocabDef
+            items = currItems + newItems
+            setItems.text = items
+        }
+        if(vocabWordBox.toString().isBlank()){
+            val hdrTxt = "Clear"
+            header.text = hdrTxt
+        }else{
+            val hdrTxt = "Create Set"
+            header.text = hdrTxt
+        }
 
 
     }
